df3087428be885d1c54af923464d7cb4bc6dd688
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-08-20 17:30:59 +0600

    totall price added and appy button enabled

--- js/calculate.js
+++ js/calculate.js
@@ -1,1 +1,27 @@
-console.log('teasting')
+let total=0;
+function addingItemCart(target){
+    const showItemTotal=document.getElementById('price-item');
+    const itemName=(target.childNodes[5].innerText);
+    const itemTitle=document.createElement('li');
+    itemTitle.innerText=itemName;
+    showItemTotal.appendChild(itemTitle);
+
+    const price=(target.childNodes[7].innerText.split(' ')[0]);
+
+    total=parseInt(total) + parseInt(price);
+    console.log(total);
+
+    document.getElementById('total').innerText=total;
+
+    const pruchasBtn=document.getElementById('purchase-btn');
+
+    if(total>0){
+        pruchasBtn.removeAttribute('disabled');
+    }
+
+    const discoutnBtn=document.getElementById('discount-btn');
+    if(total>200){
+        discoutnBtn.removeAttribute('disabled')
+    }
+}
+
