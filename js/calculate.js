b0fd63812d4dcc868623a5c4d1c199cdacdfc0ca
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-08-21 09:29:03 +0600

    purchase btn and modal added

--- js/calculate.js
+++ js/calculate.js
@@ -2,16 +2,16 @@ let total=0;
 function addingItemCart(target){
     const showItemTotal=document.getElementById('price-item');
     const itemName=(target.childNodes[5].innerText);
-    const itemTitle=document.createElement('li');
-    itemTitle.innerText=itemName;
+    const itemTitle=document.createElement('h2');
+    let count =showItemTotal.childElementCount;
+    itemTitle.innerHTML=`${count + 1}. ${itemName}`;
     showItemTotal.appendChild(itemTitle);
 
     const price=(target.childNodes[7].innerText.split(' ')[0]);
-
     total=parseInt(total) + parseInt(price);
-    console.log(total);
-
     document.getElementById('total').innerText=total;
+    // console.log(total);
+
 
     const pruchasBtn=document.getElementById('purchase-btn');
 
@@ -24,21 +24,26 @@ function addingItemCart(target){
         discoutnBtn.removeAttribute('disabled')
     }
 
-    // let discount=0;
-
     document.getElementById('discount-btn').addEventListener('click', function(){
 
         const cuponField = document.getElementById('cuponField');
         const cuponValue = cuponField.value;
-        const cupon = parseFloat(cuponValue);
 
-        if(cupon==='SELL200'){
-            const dsicountCupon= ((cupon - total)/total)*100;
-            console.log(dsicountCupon)
-
-            const discountPrice=document.getElementById('cuponField');
-            discountPrice.innerText=dsicountCupon;
+        if(cuponValue === 'SELL200'){
+            const dsicountCupon= (total/100)*20;
+            const discountPrice=document.getElementById('discount');
+            discountPrice.innerText=dsicountCupon.toFixed(2);
+            allTotalPrice= total - dsicountCupon;
+            document.getElementById('total-all').innerText=allTotalPrice;
+            // console.log(discountPrice);
         }
     })
+
+    
+
 }
+
+document.getElementById('goHome').addEventListener("click", function(){
+    window.location.href='index.html'
+})
 
